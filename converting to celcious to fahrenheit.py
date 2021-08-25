90bd5c1067ecb6e475b93f5e994bf5c2cf7fb50f
Lokesh pemmanaboina <84330548+lokesh182002@users.noreply.github.com>
2021-08-25 22:11:54 +0530

    Add files via upload

---
+++ converting to celcious to fahrenheit.py
@@ -0,0 +1,10 @@
+#python script to convert to celcious to fahrenheit.
+c=int(input('enter celcious value:'))
+print("converting to celcious to fahrenheit")
+f=(c*(9/5))+32
+print(f)
+f=float(input('enter fharenheit value:'))
+print("converting to fahrenheit to cel")
+c=(f-32)*(5/9)
+print(c)
+            
